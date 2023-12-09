a6bd642d6e8766faa975b58998927d6714062751
Thomas Durand <thomas.durand.verjat@gmail.com>
2023-12-09 17:32:30 +0100

    Solved day 9

--- README.md
+++ README.md
@@ -32,5 +32,6 @@ Some special attention will be addressed regarding performances, I'll try to sol
 | **6**  | 0,0242s          | 275724               | 37286485       |
 | **7**  | 0,0037s          | 250946742            | 251824095      |
 | **8**  | 0,0198s          | 19637                | 8811050362409  |
+| **9**  | 0,0019s          | 1887980197           | 990            |
 
 \* : Best elapsed time (after a few iterations) of the complete solution using "Release" mode on a MacBook Pro 14in 2021 with M1 Pro 10 CPU 16 GPU and 32GB of memory
--- Sources/Day09/Day09.swift
+++ Sources/Day09/Day09.swift
@@ -12,26 +12,70 @@ import Foundation
 
 import AoC
 import Common
+import Algorithms
 
 @main
 struct Day09: Puzzle {
-    typealias Input = String
-    typealias OutputPartOne = Never
-    typealias OutputPartTwo = Never
+    typealias Input = [[Int]]
+    typealias OutputPartOne = Int
+    typealias OutputPartTwo = Int
+
+    static func transform(raw: String) async throws -> [[Int]] {
+        raw.components(separatedBy: .newlines).map({ $0.components(separatedBy: .whitespaces).compactMap(Int.init) })
+    }
 }
 
 // MARK: - PART 1
 
+let example = """
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+"""
+
+private extension Array<Int> {
+    var intervals: [Int] {
+        adjacentPairs().map({ $0.1 - $0.0 })
+    }
+
+    var nextValue: Int {
+        var carry = [last.unsafelyUnwrapped]
+        var current = intervals
+        while !current.allSatisfy({ $0 == 0 }) {
+            carry.append(current.last.unsafelyUnwrapped)
+            current = current.intervals
+        }
+        var value = 0
+        while let last = carry.popLast() {
+            value += last
+        }
+        return value
+    }
+
+    var previousValue: Int {
+        var carry = [first.unsafelyUnwrapped]
+        var current = intervals
+        while !current.allSatisfy({ $0 == 0 }) {
+            carry.append(current.first.unsafelyUnwrapped)
+            current = current.intervals
+        }
+        var value = 0
+        while let last = carry.popLast() {
+            value = last - value
+        }
+        return value
+    }
+}
+
 extension Day09 {
     static var partOneExpectations: [any Expectation<Input, OutputPartOne>] {
         [
-            // TODO: add expectations for part 1
+            assert(expectation: 114, fromRaw: example)
         ]
     }
 
     static func solvePartOne(_ input: Input) async throws -> OutputPartOne {
-        // TODO: Solve part 1 :)
-        throw ExecutionError.notSolved
+        input.map({ $0.nextValue }).reduce(0, +)
     }
 }
 
@@ -40,12 +84,11 @@ extension Day09 {
 extension Day09 {
     static var partTwoExpectations: [any Expectation<Input, OutputPartTwo>] {
         [
-            // TODO: add expectations for part 2
+            assert(expectation: 2, fromRaw: example)
         ]
     }
 
     static func solvePartTwo(_ input: Input) async throws -> OutputPartTwo {
-        // TODO: Solve part 2 :)
-        throw ExecutionError.notSolved
+        input.map({ $0.previousValue }).reduce(0, +)
     }
 }
